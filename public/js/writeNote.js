6c3f6504731761a09f7daa51e389d06a6ea5aa42
John Pesavento <54077093+jpes707@users.noreply.github.com>
2021-08-03 13:20:00 -0400

    Added basic cards

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -1,10 +1,13 @@
 let googleUser = null;
 
 window.onload = () => {
+    document.querySelector("#submitButton").disabled = true;
+
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             googleUser = user;
             console.log(googleUser);
+            getItems(googleUser.uid);
             //document.querySelector("#helloTitle").innerHTML = `Hello, ${googleUser.displayName ? googleUser.displayName : googleUser.email.split('@')[0]}!`
         } else {
             document.querySelector(".hero").innerHTML = "You are not logged in.";
@@ -20,37 +23,67 @@ document.querySelector("#logoutButton").addEventListener("click", () => {
     });
 });
 
-{
-    function toggleInputCard() {
-        document.querySelector("#inputCard").classList.toggle("is-hidden");
-        document.querySelector("#inputCardToggle").classList.toggle("is-hidden");
+function toggleInputCard() {
+    document.querySelector("#inputCard").classList.toggle("is-hidden");
+    document.querySelector("#inputCardToggle").classList.toggle("is-hidden");
+}
+
+document.querySelector("#inputCardToggle").addEventListener("click", () => toggleInputCard());
+document.querySelector("#cancelButton").addEventListener("click", () => toggleInputCard());
+
+document.querySelector("#itemTitle").addEventListener("input", () => {
+    document.querySelector("#submitButton").disabled = (document.querySelector("#itemTitle").value == false);
+});
+
+document.querySelector("#submitButton").addEventListener("click", () => {
+    const payload = {
+        title: document.querySelector("#itemTitle").value,
+        text: document.querySelector("#itemDetails").value,
+        tags: document.querySelector("#itemTags").value.split(" "),
+        due: selectedDate.toString(),
+        created: new Date(Date.now()).toString()
     }
 
-    document.querySelector("#inputCardToggle").addEventListener("click", () => toggleInputCard());
-    document.querySelector("#cancelButton").addEventListener("click", () => toggleInputCard());
+    firebase.database().ref(`/users/${googleUser.uid}`).push(payload).then(() => {
+        alert("Note Submitted.");
+        document.querySelector("#itemTitle").value = "";
+        document.querySelector("#itemDetails").value = "";
+        document.querySelector("#itemTags").value = "";
+        toggleInputCard();
+    }).catch(error => {
+        console.log("error writing new note: ", error);
+    });
+});
 
-    document.querySelector("#itemTitle").addEventListener("input", () =>
-        document.querySelector("#submitButton").disabled = (document.querySelector("#itemTitle").value == false));
-
-    document.querySelector("#submitButton").addEventListener("click", () => {
-        const payload = {
-            title: document.querySelector("#itemTitle").value,
-            text: document.querySelector("#itemDetails").value,
-            tags: document.querySelector("#itemTags").value.split(" "),
-            due: selectedDate.toString(),
-            created: new Date(Date.now()).toString()
-        }
-
-        firebase.database().ref(`/users/${googleUser.uid}`).push(payload).then(() => {
-            alert("Note Submitted.");
-            document.querySelector("#itemTitle").value = "";
-            document.querySelector("#itemDetails").value = "";
-            document.querySelector("#itemTags").value = "";
-            toggleInputCard();
-        }).catch(error => {
-            console.log("error writing new note: ", error);
-        });
+const getItems = (userId) => {
+    firebase.database().ref(`/users/${userId}`).on('value', (snapshot) => {  // calls it once (on page load here), then whenever the database values change
+        renderDataAsHtml(snapshot.val());
     });
 }
 
+const renderDataAsHtml = (data) => {
+    let cards = ``;
 
+    console.log(data)
+
+    for (id in data) {
+        cards += createCard(data[id], id);
+    }
+
+    document.querySelector("#itemTable").innerHTML = cards;
+}
+
+const createCard = (item, itemId) => {
+    return `
+    <tr>
+        <td>
+            <div class="title is-4">
+                ${item.title}
+            </div>
+            <div class="subtitle is-6">
+                ${item.text}
+            </div>
+        </td>
+    </tr>
+    `;
+}
